deb9e62333bad53bdb6e7f5eecbaed330ab31a83
alinaangelsky <alina@archangelsky.us>
2023-03-02 12:45:52 -0600

    initial commit

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,36 @@
+import { useContext } from 'react'
+import { FaTimes, FaEdit} from 'react-icons/fa'
+import Card from "./shared/Card"
+import PropTypes from "prop-types"
+import FeedbackContext from '../context/FeedbackContext'
+
+function FeedbackItem({item}) {
+//  const [rating, setRating] = useState(7)
+//  const [text, setText] = useState('This is an example of a feedback item')
+//  const handleClick = () => {
+//     setRating((prev)=>{
+//         return prev + 1
+//     })
+//  }
+const {deleteFeedback, editFeedback} = useContext(FeedbackContext)
+
+  return (
+    <Card >
+      <div className="num-display">{item.rating}</div>
+      <button onClick={() => deleteFeedback(item.id)} 
+      className='close'>
+        <FaTimes color='purple'/>
+      </button>
+      <button onClick={() => editFeedback(item)} className="edit">
+      <FaEdit color='purple'/>
+      </button>
+      <div className="text-display">{item.text}</div>
+      {/* <button onClick={handleClick}>Click</button> */}
+    </Card>
+  )
+}
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
+}
+
+export default FeedbackItem
---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,34 @@
+// import PropTypes from "prop-types"
+import { useContext } from "react"
+import FeedbackItem from './FeedbackItem'
+import FeedbackContext from "../context/FeedbackContext"
+
+// NOTE: added layout prop for nicer animation
+// https://www.framer.com/docs/animation/#layout-animations
+
+function FeedbackList() {
+  const {feedback} = useContext(FeedbackContext)
+    if(!feedback || feedback.length === 0){
+        return <p>'No Feedback Yet'</p>
+    }
+  
+  return (
+    <div className="feedback-list">
+      {feedback.map((item)=>(
+        <FeedbackItem key={item.id} item={item}
+        />
+      ))}
+    </div>
+  )
+}
+// FeedbackList.propTypes = {
+// feedback: PropTypes.arrayOf(
+//   PropTypes.shape({
+//     id: PropTypes.number.isRequired,
+//     text: PropTypes.string.isRequired,
+//     rating: PropTypes.number.isRequired,
+//     })
+//   ),
+// }
+
+export default FeedbackList
